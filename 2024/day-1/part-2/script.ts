a6a2401166efa3ccdb7db1ded877805ef202aa0c
Layton Berth <laytonbrth@gmail.com>
2024-12-11 00:19:35 +0100

    finish part 2

---
+++ 2024/day-1/part-2/script.ts
@@ -0,0 +1,20 @@
+import { readFile } from "../../utils.ts";
+
+const lines = await readFile("./input.txt");
+
+const left: number[] = [];
+const right: number[] = [];
+let sum: number = 0;
+
+for (const line of lines) {
+  const [l, r] = line.split("   ").map(Number);
+  left.push(l);
+  right.push(r);
+}
+
+left.forEach(number => {
+    const occurence = right.filter((x) => x == number).length;
+    sum += (number * occurence)
+})
+
+console.log(sum);
