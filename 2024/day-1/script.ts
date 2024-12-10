4263a40a2b71ecc5c7557017cf10560af3173f46
Layton Berth <laytonbrth@gmail.com>
2024-12-11 00:04:27 +0100

    feat: parse lines from test_input.txt into left and right arrays in day-1 script

--- 2024/day-1/script.ts
+++ 2024/day-1/script.ts
@@ -1,4 +1,14 @@
 import { readFile } from '../utils.ts'
 
 const lines = await readFile('./test_input.txt');
-console.log(lines);
+
+const left: number[] = [];
+const right: number[] = [];
+
+for (const line of lines) {
+  const [l, r] = line.split('   ').map(Number);
+  left.push(l);
+  right.push(r);
+}
+
+console.log(left, right);
