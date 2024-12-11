04c144c10a4f96c85106e17c013b7293b88e0a05
Layton Berth <laytonbrth@gmail.com>
2024-12-11 01:19:52 +0100

    finish

--- 2024/day-2/part-2/script.ts
+++ 2024/day-2/part-2/script.ts
@@ -7,31 +7,41 @@ lines.forEach((line) => {
   const numbers = line.split(" ").map(Number);
 
   if (
-    !isAscending(numbers) && !isDescending(numbers) ||
+    (!isAscending(numbers) && !isDescending(numbers)) ||
     !isValidLevelAdjacentDifference(numbers)
   ) {
-    reportsSafe--;
+    let canBeMadeSafe = false;
+
+    for (let i = 0; i < numbers.length; i++) {
+      const modifiedArray = numbers.slice(0, i).concat(numbers.slice(i + 1));
+
+      if (
+        (isAscending(modifiedArray) || isDescending(modifiedArray)) &&
+        isValidLevelAdjacentDifference(modifiedArray)
+      ) {
+        canBeMadeSafe = true;
+        break;
+      }
+    }
+
+    if (!canBeMadeSafe) reportsSafe--;
   }
 });
 
 console.log(reportsSafe);
 
-//Helpers
+// Helpers
 
 function isAscending(array: Array<number>): boolean {
-  return array.every(function (x: number, i: number) {
-    return i === 0 || x >= array[i - 1];
-  });
+  return array.every((x, i) => i === 0 || x >= array[i - 1]);
 }
 
 function isDescending(array: Array<number>): boolean {
-  return array.every(function (x: number, i: number) {
-    return i === 0 || x <= array[i - 1];
-  });
+  return array.every((x, i) => i === 0 || x <= array[i - 1]);
 }
 
 function isValidLevelAdjacentDifference(array: Array<number>): boolean {
-  return array.every(function (x: number, i: number) {
+  return array.every((x, i) => {
     if (i === 0) return true;
     const difference = Math.abs(x - array[i - 1]);
     return difference >= 1 && difference <= 3;
