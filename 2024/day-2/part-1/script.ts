fae0be419ca10d9bf0b461863f49c637a9daf82f
Layton Berth <laytonbrth@gmail.com>
2024-12-11 01:20:20 +0100

    update helpers

--- 2024/day-2/part-1/script.ts
+++ 2024/day-2/part-1/script.ts
@@ -19,19 +19,15 @@ console.log(reportsSafe);
 //Helpers
 
 function isAscending(array: Array<number>): boolean {
-  return array.every(function (x: number, i: number) {
-    return i === 0 || x >= array[i - 1];
-  });
+  return array.every((x, i) => i === 0 || x >= array[i - 1]);
 }
 
 function isDescending(array: Array<number>): boolean {
-  return array.every(function (x: number, i: number) {
-    return i === 0 || x <= array[i - 1];
-  });
+  return array.every((x, i) => i === 0 || x <= array[i - 1]);
 }
 
 function isValidLevelAdjacentDifference(array: Array<number>): boolean {
-  return array.every(function (x: number, i: number) {
+  return array.every((x, i) => {
     if (i === 0) return true;
     const difference = Math.abs(x - array[i - 1]);
     return difference >= 1 && difference <= 3;
