78c19c82c7f52fe7cc9be953fa47ce0ce060207f
Layton Berth <laytonbrth@gmail.com>
2024-12-11 01:10:29 +0100

    remove comments

--- 2024/day-2/part-1/script.ts
+++ 2024/day-2/part-1/script.ts
@@ -1,16 +1,3 @@
-/*
-- The levels are either all increasing or all decreasing.
-- Any two adjacent levels differ by at least one and at most three.
-*/
-/*
-[ "7", "6", "4", "2", "1" ]
-[ "1", "2", "7", "8", "9" ]
-[ "9", "7", "6", "2", "1" ]
-[ "1", "3", "2", "4", "5" ]
-[ "8", "6", "4", "4", "1" ]
-[ "1", "3", "6", "7", "9" ]
- */
-
 import { readFile } from "../../utils.ts";
 
 const lines = await readFile("./input.txt");
